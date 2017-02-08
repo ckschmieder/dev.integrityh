cd245e9653b8ca9b83115ff258abc5399335fd25
Chris <ckschmieder@gmail.com>
2017-02-08 15:41:52 -0500

    fixed element id in archove php

--- archive.php
+++ archive.php
@@ -20,25 +20,26 @@ if (is_post_type_archive()) {
 
 <div class="wrapper">
   <div class="container">
-    <div id="main-content archive">
-      <header class="entry-header">
-        <h1 class="page-title"><?php echo $title; ?></h1>
-      </header>
+    <div id="main-content" class="archive">
+      <div class="indent">
+        <header class="entry-header">
+          <h1 class="page-title"><?php echo $title; ?></h1>
+        </header>
 
-      <?php if (have_posts()): ?>
-        <ol class="search-results">
-          <?php while (have_posts()): the_post(); ?>
-            <?php get_template_part('search-result'); ?>
-          <?php endwhile; ?>
-        </ol>
+        <?php if (have_posts()): ?>
+          <ol class="search-results">
+            <?php while (have_posts()): the_post(); ?>
+              <?php get_template_part('search-result'); ?>
+            <?php endwhile; ?>
+          </ol>
 
-        <?php ih_pagination_links(); ?>
-      <?php else: ?>
-        <p>
-          <em>No results found</em>
-        </p>
-      <?php endif; ?>
-
+          <?php ih_pagination_links(); ?>
+        <?php else: ?>
+          <p>
+            <em>No results found</em>
+          </p>
+        <?php endif; ?>
+      </div>
     </div>
     <?php get_sidebar(); ?>
   </div>
