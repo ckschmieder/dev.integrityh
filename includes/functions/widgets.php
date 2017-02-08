a254e8555ab4dbace604f1a05ae4652fad8d471f
Chris <ckschmieder@gmail.com>
2017-02-08 13:12:30 -0500

    Added custom Upcoming Events widget for sidebar

--- includes/functions/widgets.php
+++ includes/functions/widgets.php
@@ -62,4 +62,122 @@ function randj_widgets_init() {
 }
 add_action( 'widgets_init', 'randj_widgets_init' );
 
+
+// Creating the widget 
+class wpb_widget extends WP_Widget {
+
+function __construct() {
+parent::__construct(
+// Base ID of your widget
+'wpb_widget', 
+
+// Widget name will appear in UI
+__('Upcoming Events Widget loop', 'wpb_widget_domain'), 
+
+// Widget description
+array( 'description' => __( 'Auto updated list of upcoming events based on event date in the future', 'wpb_widget_domain' ), ) 
+);
+}
+
+// Creating widget front-end
+// This is where the action happens
+public function widget( $args, $instance ) {
+$title = apply_filters( 'widget_title', $instance['title'] );
+// before and after widget arguments are defined by themes
+echo $args['before_widget'];
+if ( ! empty( $title ) )
+echo $args['before_title'] . $title . $args['after_title'];
+
+// This is where you run the code and display the output
+/*echo __( 'Hello, World!', 'wpb_widget_domain' );
+echo $args['after_widget'];*/
+
+$events_archive = get_post_type_archive_link('event');
+            $events_query = new WP_Query(array(
+              'post_type'      => 'event',
+              'post_status'    => 'publish',
+              'posts_per_page' => 3,
+              'order'          => 'ASC',
+              'orderby'        => 'meta_value',
+              'meta_key'       => 'start_date',
+              'meta_query'     => array(
+                array(
+                  'key'     => 'start_date',
+                  'value'   => strftime('%Y-%m-%d'),
+                  'compare' => '>=',
+                  'type'    => 'DATE',
+                ),
+              ),
+            ));
+            ?>
+
+            <ul class="sidebar-events">
+
+            <?php
+            
+            // The Loop
+            if ( $events_query->have_posts() ) {
+              while ( $events_query->have_posts() ) {
+                $events_query->the_post();
+                ?>
+
+               
+                  
+
+                  <!-- <div class="event-info"> -->
+
+                    
+                    <li><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></li>
+                    
+
+                  <!-- </div> -->
+
+                  <?php
+
+
+                    }
+                  } else {
+                    // no posts found
+                  }
+
+                  // Restore original Post Data
+                  wp_reset_postdata();
+                  ?>
+                  </ul>
+                  <a href="/events/" class="sidebar-link">More Events »</a>
+                  </div>
+                <?php
+
+}
+        
+// Widget Backend 
+public function form( $instance ) {
+if ( isset( $instance[ 'title' ] ) ) {
+$title = $instance[ 'title' ];
+}
+else {
+$title = __( 'New title', 'wpb_widget_domain' );
+}
+// Widget admin form
+?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label> 
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<?php 
+}
+    
+// Updating widget replacing old instances with new
+public function update( $new_instance, $old_instance ) {
+$instance = array();
+$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+return $instance;
+}
+} // Class wpb_widget ends here
+
+// Register and load the widget
+function wpb_load_widget() {
+    register_widget( 'wpb_widget' );
+}
+add_action( 'widgets_init', 'wpb_load_widget' );
 ?>
